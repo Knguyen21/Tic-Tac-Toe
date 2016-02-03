9749e1bd36f819dcc8061c80ca321a0015900e9a
khanhnhat <nguyen.k21@gmail.com>
2016-02-03 10:59:53 -0500

    Update code to display the winner/ tie.

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let players = ['X', 'O'];
+let players = ['<img src="http://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/128/apple-red-icon.png"/>', '<img src="http://pngimg.com/upload/orange_PNG766.png">', '<img src= "http://www.altitudementalhealth.com/wp-content/uploads/2014/01/Apple-and-Orange-tied-together-7509690-300x200.jpg"/>'];
 let count = 0;
 let gameboard =['', '', '', '', '', '', '', '', ''];
 let playerScore = [0,0];
@@ -11,16 +11,21 @@ let tieScore = 0;
 const reset = function(){
   count = 0;
   gameboard =['', '', '', '', '', '', '', '', ''];
+  $('#winner').hide();
   $('.board').children().html("");
+  // $('.board').show();
+  $('#announcement').html("Player 1: Apple turn!").show();
 
 };
 
 const tie = function(){
   if(count === 9){
     tieScore += 1;
+    // $('.board').hide();
+    $('#winner').html('Oh No! A Tie!' + players[2]).show();
     $('#tie').html(tieScore);
-    alert("tie");
-    reset();
+    $('#announcement').hide();
+
   }
 };
 
@@ -36,7 +41,10 @@ const getWinner = function(player, value){
     ((gameboard[0] === value) && (gameboard[4] === value) &&(gameboard[8] === value)) ||
     ((gameboard[2] === value) && (gameboard[4] === value) &&(gameboard[6] === value)) )
   {
-      alert("winner is " + players[player]);
+      // $('.board').hide();
+      $('#announcement').hide();
+      $('#winner').html("winner is " + players[player]).show();
+
       playerScore[player] += 1;
       if(value ==='x'){
         $('#x').html(playerScore[player]);
@@ -44,13 +52,15 @@ const getWinner = function(player, value){
       else if(value ==='o'){
         $('#o').html(playerScore[player]);
       }
-      reset();
+
+
+
   }
 };
 
 
 $(document).ready(function() {
-
+  $('#winner').hide();
     $('.board').children().click(function() {
       if($(this).html() === '') {
         if(count % 2 === 0){
@@ -59,6 +69,7 @@ $(document).ready(function() {
           gameboard[event.target.id] ='x';
           // alert(event.target.id);
           console.log(gameboard);
+          $('#announcement').html("Player 2: Orange turn!");
           getWinner(0, 'x');
           tie();
 
@@ -69,6 +80,7 @@ $(document).ready(function() {
           gameboard[event.target.id] ='o';
           // alert(event.target.id);
           console.log(gameboard);
+          $('#announcement').html("Player 1: Apple turn!");
           getWinner(1, 'o');
           tie();
         }
