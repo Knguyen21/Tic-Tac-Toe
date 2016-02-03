d0a07b7c0fb3620a492391687a9d40b62aa98664
khanhnhat <nguyen.k21@gmail.com>
2016-02-03 11:57:20 -0500

    fix counting error

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -6,6 +6,9 @@ let gameboard =['', '', '', '', '', '', '', '', ''];
 let playerScore = [0,0];
 let tieScore = 0;
 
+let game = {
+  over: false,
+};
 
 
 const reset = function(){
@@ -25,13 +28,14 @@ const tie = function(){
     $('#winner').html('Oh No! A Tie!' + players[2]).show();
     $('#tie').html(tieScore);
     $('#announcement').hide();
+    game.over =true;
 
   }
 };
 
 
 
-const getWinner = function(player, value){
+const getWinner = function(player, value, game){
   if(((gameboard[0] === value) && (gameboard[1] === value) &&(gameboard[2] === value)) ||
     ((gameboard[3] === value) && (gameboard[4] === value) &&(gameboard[5] === value)) ||
     ((gameboard[6] === value) && (gameboard[7] === value) && (gameboard[8] === value)) ||
@@ -48,48 +52,53 @@ const getWinner = function(player, value){
       playerScore[player] += 1;
       if(value ==='x'){
         $('#x').html(playerScore[player]);
+        game.over = true;
       }
       else if(value ==='o'){
         $('#o').html(playerScore[player]);
+        game.over = true;
       }
-
-
-
+    count = 0;
   }
 };
 
 
 $(document).ready(function() {
   $('#winner').hide();
-    $('.board').children().click(function() {
+
+  $('.board').children().click(function() {
+    if(game.over === false) {
       if($(this).html() === '') {
         if(count % 2 === 0){
           $(this).html(players[0]);
-          count +=1;
           gameboard[event.target.id] ='x';
           // alert(event.target.id);
           console.log(gameboard);
           $('#announcement').html("Player 2: Orange turn!");
-          getWinner(0, 'x');
+          getWinner(0, 'x', game);
+          count +=1;
           tie();
 
         }
         else {
           $(this).html(players[1]);
-          count +=1;
           gameboard[event.target.id] ='o';
           // alert(event.target.id);
           console.log(gameboard);
           $('#announcement').html("Player 1: Apple turn!");
-          getWinner(1, 'o');
+          getWinner(1, 'o', game);
+          count +=1;
           tie();
         }
       }
-    });
+    }
+  });
+
 
 
     $('#reset').click(function() {
       reset();
+      game.over = false;
     });
 });
 module.exports = true;
